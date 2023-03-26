c2de39d89e3531a65469d461f1f7af639099ecf2
IT22255488 <128682795+IT22255488@users.noreply.github.com>
2023-03-27 05:29:07 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -13,6 +13,16 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+      int n,i,sum=0;
+    
+    printf("enter a number");
+    scanf("%d",&n);
+    
+    for(i=1;i<=n;i++){
+    	sum+=i;
+	}
+	printf("%d",sum);
+   
   
   return 0;
 }
